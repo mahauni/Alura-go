7e1cb4f96ebbadffc3a35e253250f2cfdc19183b
Lucas Raoni <lucasraoniha@gmail.com>
2023-03-03 20:26:31 -0300

    Fixed database code

--- RestAPI-Go/database/db.go
+++ RestAPI-Go/database/db.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"log"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,5 +14,8 @@ var (
 
 func ConectaComBancoDeDados() {
 	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+	if err != nil {
+		log.Panic("Erro ao conectar com o banco de dados")
+	}
 }
