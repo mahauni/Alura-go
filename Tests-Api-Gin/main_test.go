5ee4ee9556a8b0542e892d2f0ae4805a565fe5a9
Lucas Raoni <lucasraoniha@gmail.com>
2023-03-17 15:13:59 -0300

    Added testify in the tests

--- Tests-Api-Gin/main_test.go
+++ Tests-Api-Gin/main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/mahauni/Alura-go/Tests-Api-Gin/controllers"
+	"github.com/stretchr/testify/assert"
 )
 
 func SetupDasRotasDeTeste() *gin.Engine {
@@ -20,7 +22,8 @@ func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/gui", nil)
 	resposta := httptest.NewRecorder()
 	r.ServeHTTP(resposta, req)
-	if resposta.Code != http.StatusOK {
-		t.Fatalf("Status error: valor recebido foi %d e o esperado era %d", resposta.Code, http.StatusOK)
-	}
+	assert.Equal(t, http.StatusOK, resposta.Code, "They should be equal")
+	mockDaResposta := `{"API diz:":"E ai gui, tudo beleza?"}`
+	respostaBody, _ := ioutil.ReadAll(resposta.Body)
+	assert.Equal(t, mockDaResposta, string(respostaBody))
 }
